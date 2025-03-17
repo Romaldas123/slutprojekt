09e2a9c8403ae7969eb4558d4c18ed7df6b5e9eb
Romaldas123 <romaldas.blyza@skola.taby.se>
2025-03-17 17:35:52 +0100

    Add files via upload

--- Slutprojekt/slutprojekt.js
+++ Slutprojekt/slutprojekt.js
@@ -9,4 +9,22 @@ document.addEventListener("DOMContentLoaded", function () {
             this.style.backgroundColor = "#dcdcdc";
         });
     });
+
+    let lastScrollTop = 0;
+    const header = document.getElementById("header");
+    const headerHeight = header.offsetHeight; 
+
+    window.addEventListener("scroll", function () {
+        let currentScroll = window.scrollY;
+
+        if (currentScroll > headerHeight) {
+            if (currentScroll > lastScrollTop) {
+                header.style.top = `-${headerHeight}px`;
+            } else {
+                header.style.top = "0";
+            }
+        }
+
+        lastScrollTop = currentScroll;
+    });
 });
